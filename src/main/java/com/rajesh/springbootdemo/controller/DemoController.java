b24d56da7964164caee9104d8ab183a26d33749e
JG <g.rajesh1234@gmail.com>
2022-12-04 00:28:03 -0600

    Test branch (#2)
    * updated version
    
    * added new end point
    
    Co-authored-by: snigdha B <snigdhai136@gmail.com>

--- src/main/java/com/rajesh/springbootdemo/controller/DemoController.java
+++ src/main/java/com/rajesh/springbootdemo/controller/DemoController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/greeting/{name}")
-    public String getGreeting(@PathVariable("name") String name){
-        return "Hello " + name;
+    @GetMapping("/greeting/{name}/message/{msg}")
+    public String getGreeting(@PathVariable("name") String name, @PathVariable("msg") String msg){
+        return "Hello " + name + " " + msg;
     }
 }
